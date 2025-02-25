1c76b3a1e1884813011a21b931c483b36f3bf120
Kate <bogomolnayaa@bk.ru>
2025-02-25 23:50:11 +0300

    correct

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -3,6 +3,8 @@ package jm.task.core.jdbc;
 import jm.task.core.jdbc.model.User;
 import jm.task.core.jdbc.service.UserService;
 import jm.task.core.jdbc.service.UserServiceImpl;
+import jm.task.core.jdbc.util.Util;
+
 import java.util.List;
 
 public class Main {
@@ -22,5 +24,7 @@ public class Main {
 
         userService.cleanUsersTable();
         userService.dropUsersTable();
+
+        Util.closeConnection();
     }
 }
