0fe3c5a6ac17d5b0116a4e6aff4f3830e8d6636b
Kate <bogomolnayaa@bk.ru>
2025-02-25 23:48:29 +0300

    correct

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,11 +1,22 @@
 package jm.task.core.jdbc.util;
 
+import jm.task.core.jdbc.model.User;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.cfg.Environment;
+import org.hibernate.service.ServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+
+
 import java.sql.*;
+import java.util.Map;
+import java.util.Properties;
 
 public class Util {
     private static final String USERNAME = "root";
     private static final String PASSWORD = "root";
     private static final String URL = "jdbc:mysql://localhost:3306/db1";
+    private static final String DRIVER = "com.mysql.jdbc.Driver";
 
     public static Connection connect() {
         Connection connection = null;
@@ -15,6 +26,16 @@ public class Util {
             System.err.println(e.getMessage());
         }
         return connection;
+    }
+
+    public static void closeConnection() {
+        if (connect() != null) {
+           try {
+               connect().close();
+           } catch (SQLException e) {
+               e.printStackTrace();
+           }
+        }
     }
 }
 
