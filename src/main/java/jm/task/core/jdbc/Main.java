e46cecf19cfcee9b02b55e007a6bdceaff0ad256
bogomolnayaa <85729535+bogomolnayaa@users.noreply.github.com>
2025-02-26 14:16:27 +0300

    Update

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -24,6 +24,6 @@ public class Main {
         userService.cleanUsersTable();
         userService.dropUsersTable();
 
-        Util.closeConnection();
+        Util.closeConnection(UserDaoJDBCImpl.connection);
     }
 }
